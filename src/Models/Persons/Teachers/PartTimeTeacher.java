5ec391ee5769f8e408cb9992bc4d66c27c0495f2
J-Potes-GLB <juan.potes@globant.com>
2024-02-14 15:56:15 -0500

    Added comments in the Person, FullTimeTeacher and PartTimeTeacher class files

--- src/Models/Persons/Teachers/PartTimeTeacher.java
+++ src/Models/Persons/Teachers/PartTimeTeacher.java
@@ -14,13 +14,18 @@ public class PartTimeTeacher extends Teacher {
 
     // Methods
 
+    // Method to increase the weekly active hours of the part-time teacher
     public void increaseActiveHours(int additionalHours){
         this.activeHoursPerWeek += additionalHours;
     }
 
+    // Method to calculate the salary of part-time teachers, in this case, the base salary is multiplied by the amount of hours worked in the week.
+    // The amount of hours worked depends on the classes assigned to the teacher.
     public void calculateSalary(){
         this.fullSalaryPerWeek = this.baseSalaryPerHour * this.activeHoursPerWeek;
     }
+
+    // Method to show the details of the teacher (The index is a parameter to only show the titles with the first element of a list and not with the rest)
     public void showDetails(int index){
         if(index == 0){
             System.out.println(String.format("%-10s %-20s %-20s %-20s %-20s %-20s %-20s", "ID", "NAME", "TYPE", "EXPERIENCE YEARS", "BASE SALARY/HOUR", "HOURS/WEEK", "SALARY/WEEK"));
